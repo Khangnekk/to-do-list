ac946174885ae5cbc110feab23c89aa17f8371f1
Khang Nguyen Luong <nlk@aod.vn>
2025-05-26 10:58:37 +0700

    revert

---
+++ src/utils/lang.js
@@ -0,0 +1,26 @@
+const LANG = {
+  en: {
+    settingTitle: "Settings",
+    changeName: "Change Name",
+    darkMode: "Dark Mode",
+    darkOn: "On",
+    darkOff: "Off",
+    language: "Language",
+    vi: "Vietnamese",
+    en: "English",
+    warning: "Warning: Changes will be saved automatically."
+  },
+  vi: {
+    settingTitle: "Cài đặt",
+    changeName: "Thay đổi tên",
+    darkMode: "Chế độ tối",
+    darkOn: "Bật",
+    darkOff: "Tắt",
+    language: "Ngôn ngữ",
+    vi: "Tiếng Việt",
+    en: "Tiếng Anh",
+    warning: "Cảnh báo: Thay đổi sẽ được lưu tự động."
+  }
+};
+
+export default LANG; 
